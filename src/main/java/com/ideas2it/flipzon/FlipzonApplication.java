d2d707f12fe5b3a37000faf790a3ea3fab39dfb7
JeevithaKesavaraj <jeevithakesavaraj2001@gmail.com>
2024-08-21 16:42:30 +0530

    Feat: flipzon added

---
+++ src/main/java/com/ideas2it/flipzon/FlipzonApplication.java
@@ -0,0 +1,13 @@
+package com.ideas2it.flipzon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipzonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipzonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideas2it/flipzon/FlipzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideas2it.flipzon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
