93ff94c806dfd7aba11811f8b99c84a3e07c3792
GowthamSrt <gowthasrt@yahoo.com>
2024-08-26 17:51:31 +0530

    pom xml updated and removed some unwanted files

--- src/main/java/com/ideas2it/flipzon/dao/DeliveryDao.java
+++ src/main/java/com/ideas2it/flipzon/dao/DeliveryDao.java
@@ -1,6 +1,5 @@
 package com.ideas2it.flipzon.dao;
 
-import com.ideas2it.flipzon.model.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeliveryDao extends JpaRepository<Delivery, Long> {
