c5a07588a4f2ac7a60c6560a684d97f0b9a0244b
GowthamSrt <gowthasrt@yahoo.com>
2024-08-27 10:49:54 +0530

    Updated wishlist and cart

--- src/main/java/com/ideas2it/flipzon/dto/WishlistDto.java
+++ src/main/java/com/ideas2it/flipzon/dto/WishlistDto.java
@@ -1,7 +1,10 @@
 package com.ideas2it.flipzon.dto;
 
+import com.ideas2it.flipzon.model.Product;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.List;
 
 /**
  * <p>
@@ -15,5 +18,6 @@ import lombok.Data;
 @Data
 public class WishlistDto {
     private long id;
+    private List<Product> products;
     private long customerId;
 }
