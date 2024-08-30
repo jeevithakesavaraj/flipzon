4931d1b004e3f8bbc09225a6872f2502ebb88481
GowthamSrt <gowthasrt@yahoo.com>
2024-08-30 15:12:55 +0530

    Logger added
    # Conflicts:
    #	logs/app.log
    #	pom.xml
    #	src/main/resources/log4j2.yml

--- src/main/java/com/ideas2it/flipzon/FlipzonApplication.java
+++ src/main/java/com/ideas2it/flipzon/FlipzonApplication.java
@@ -1,5 +1,7 @@
 package com.ideas2it.flipzon;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,10 +9,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class FlipzonApplication {
-
+    private static final Logger logger = LogManager.getLogger(FlipzonApplication.class);
 	public static void main(String[] args) {
 
 		SpringApplication.run(FlipzonApplication.class, args);
+		logger.info("Application Started");
 	}
 
 }
